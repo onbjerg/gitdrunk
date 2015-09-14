6e3956ad69ca6a41aef9186a21b3077ab2c1bca6
onbjerg <oliver@pfmedia.dk>
2015-09-14 20:50:03 +0200

    Be more polite

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# What the fuck is gitdrunk?
+## gitdrunk
+> "[Break the Ballmer Peak](https://xkcd.com/323/)"
+
 gitdrunk is a git game designed to make you a better developer and alcoholic by coding drunk. Hell, it might even make you a better person.
 
-The game makes those lonely weekend nights spent programming and sipping your shitty, domestic beer less suicidal.
+The game makes those lonely weekend nights spent programming and sipping your domestic beer less suicidal.
 
-# How do I play?
-It's easy. All the rules are embedded in the program. Fun, right? No.
+## How do I play?
+It's easy. All the rules are embedded in the program.
 
-In order to play, just install the game and use ``gitd`` instead of ``git``.
+In order to play, just install the game and use ``gitd`` instead of ``git``. All standard git commands work!
 
-# Well, what are you waiting for?
+## Well, what are you waiting for?
 Install it using ``npm install -g gitdrunk``. You know you want to.
--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 var shell = require('shelljs')
 
 if (!shell.which('git')) {
-  console.error('GITdrunk requires git. How sensational!')
+  console.error('gitdrunk requires git.')
 }
 
 var args = process.argv.splice(2)
 var subcommand = args[0]
 
 var rules = {
-  'clone': 'Chug your drink, you unoriginal, copying, lazy piece of shit!',
+  'clone': 'How unoriginal. Chug your drink.',
   'pull': 'Good for you. You get a pass.',
   'branch': 'Drink. Ask yourself if the branch is justifiable and drink again.',
   'checkout': 'You get a pass for acknowledging good git practice!',
-  'push': 'Fuck you. Drink two!',
-  'revert': 'Take a shot. You suck, bro.',
-  'reset': 'Take a shot. You suck, bro.',
-  'rebase': 'I said code and drink, not fuck your repo till it bleeds. Take two shots, you zero.'
+  'push': 'Don\'t do that. Drink two!',
+  'revert': 'Take a shot. You suck.',
+  'reset': 'Take a shot. You suck.',
+  'rebase': 'I said code and drink, not fuck your repo \'till it bleeds. Take two shots.'
 }
 
 var cmd_string = args.map(function (arg) {
